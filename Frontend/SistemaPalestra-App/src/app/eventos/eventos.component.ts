cac16680c36ffe070f0fa8d3440932bc9ef920c6
ThiagoPolli <thiagopolli.palma@gmail.com>
2022-05-19 18:02:18 -0300

    Adicionado busca por tema e local frontend

--- Frontend/SistemaPalestra-App/src/app/eventos/eventos.component.ts
+++ Frontend/SistemaPalestra-App/src/app/eventos/eventos.component.ts
@@ -8,19 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any;
-
   URL : string = 'http://localhost:5000/api/eventos'
+  public eventos : any =[];
+  public eventoFiltrados: any = [];
+  widthImage = 150;
+  marginImage =2;
+  showImage = true;
+  private _filterList: string = '';
+
+
+  public get filterList(): string{
+    return this._filterList;
+  }
+  public set filterList(value: string){
+    this._filterList = value;
+    this.eventoFiltrados = this.filterList ? this.filterEventos(this.filterList) : this.eventos;
+  }
+
+
+  filterEventos(filterPor: string): any{
+    filterPor = filterPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema:string; local:string; }) => 
+        evento.tema.toLocaleLowerCase().indexOf(filterPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filterPor) !== -1
+
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
   }
+  hidenImage(){
+    this.showImage = !this.showImage;
+  }
 
   public getEventos(): void {
     this.http.get(this.URL).subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventoFiltrados = this.eventos
+      },
       error => console.log(error)
     );
   }
